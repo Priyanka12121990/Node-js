a50fd0e53b373d1ae42cef3e5aaf1d86f7687c7e
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-05 15:49:16 +0530

    new task added

---
+++ example9.js
@@ -0,0 +1,20 @@
+var fs = require("fs");
+
+fs.readFile('file1data.txt','utf8', function(err1, data1){
+    fs.readFile('file2data.txt','utf8',function(err2, data2){
+            
+            if(err1 || err2){
+                   throw new Error();
+            }
+  
+            let data = data1+data2;
+            
+            fs.writeFile("file3.txt", data , (err) => {
+    			if (err) {
+        			console.error(err);	
+        			return;
+    			};
+    			console.log("2 file data added into a single file");
+			});
+    });
+});
