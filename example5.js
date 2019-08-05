41cd34f6ab9280c778ea5bafc10129103141eba5
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-05 13:28:22 +0530

    delete5

--- example5.js
+++
@@ -1,7 +0,0 @@
-//Example 5
-
-var cal = require('./calModule.js');
-console.log("Addition = " + cal.add(5,6));
-console.log("Substraction =" + cal.sub(7,6));
-console.log("Multiplication =" + cal.mul(5,6));
-console.log("Division = " + cal.div(10,5));
