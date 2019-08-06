a52eb570e180e31622a2d9ff9da3c346098deec2
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-06 11:27:39 +0530

    new task

---
+++ example13.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+
+app.get('/show', function (req, res) {
+  res.status(200).send({"id" : req.query.id,"name": req.query.name});
+})
+
+
+app.get("*",(req,res)=>{
+     	res.status(404).send( {"Message": "invalid information"});
+});
+	
+
+
+app.listen(3000,(rep) => {
+  
+});
+
+
