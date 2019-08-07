6f1be8418c1162f5db16aa5109bdb42446df6258
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-07 11:34:57 +0530

    new file

---
+++ example15.js
@@ -0,0 +1,26 @@
+const express = require("express");
+var bodyParser = require('body-parser');
+var formidableMiddleware = require('express-formidable');
+
+const app = express();
+app.use(bodyParser.json()); //json raw
+app.use(bodyParser.urlencoded({ extended: true })); // encoding
+app.use(formidableMiddleware());
+
+app.post('/', (req, res) => {
+  res.send(req.body);
+  //res.send(req.body.foo);
+
+})
+
+app.post('/form', (req, res) => {
+  //req.fields; // contains non-file fields
+  //req.files; // contains files
+
+  res.send(req.fields);
+
+});
+
+app.listen("3000",(rep) => {
+  console.log("server connected");
+});
