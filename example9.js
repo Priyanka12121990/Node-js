756e0290cd0ddbc72a0885aeb2ab9c1ccfa9da94
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-09 15:03:24 +0530

    Coding Indentation

--- example9.js
+++ example9.js
@@ -1,20 +1,17 @@
 var fs = require("fs");
 
-fs.readFile('file1data.txt','utf8', function(err1, data1){
-    fs.readFile('file2data.txt','utf8',function(err2, data2){
-            
-            if(err1 || err2){
+fs.readFile('file1data.txt', 'utf8', function(err1, data1) {
+    fs.readFile('file2data.txt', 'utf8', function(err2, data2) {
+            if(err1 || err2) {
                    throw new Error();
             }
-  
-            let data = data1+data2;
-            
-            fs.writeFile("file3.txt", data , (err) => {
-    			if (err) {
-        			console.error(err);	
-        			return;
-    			};
-    			console.log("2 file data added into a single file");
-			});
-    });
+ 	    let data = data1+data2;
+            fs.writeFile("file3.txt", data, (err) => {
+    		if (err) {
+        		console.error(err);	
+        		return;
+    		};
+    		console.log("2 file data added into a single file");
+	    });
+     });
 });
