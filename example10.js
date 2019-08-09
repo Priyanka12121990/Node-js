8ddf1ebdae00328bfdc22794725c25717ba1ef22
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-09 13:22:01 +0530

    Coading Indentation

--- example10.js
+++ example10.js
@@ -1,4 +1,4 @@
-function calculator(number_one,number_two){
+function calculator(number_one,number_two) {
 	this.number_one = parseInt(number_one);
 	this.number_two = parseInt(number_two);
 }
@@ -19,7 +19,7 @@ calculator.prototype.div = function() {
 	return this.number_one / this.number_one;
 } 
 
-var cal_obj = new calculator(20,10);
+var cal_obj = new calculator(20, 10);
 console.log(cal_obj.add());
 console.log(cal_obj.sub());
 console.log(cal_obj.mul());
