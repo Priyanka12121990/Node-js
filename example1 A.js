64b40c6159cf8438f6ce00a7eda90b997073c87e
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-09 13:17:14 +0530

    coding indentation

--- example1 A.js
+++ example1 A.js
@@ -1,10 +1,10 @@
 // Example 1A using node js
 
-function placeOrder(ex_time, order_id){
+function placeOrder(ex_time, order_id) {
 	console.log(`receive order`+ order_id);
-	setTimeout(()=>{
+	setTimeout(() => {
 		console.log("delivery order"+ order_id);
-	},ex_time);
+	}, ex_time);
 }
 placeOrder(5000, 1);
 placeOrder(4000, 2);
