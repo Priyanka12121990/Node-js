de8c55af6cefad3bdbef7d0de4e4d1ba13dccf7a
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-09 14:58:08 +0530

    Coding indentation

--- example5.js
+++ example5.js
@@ -1,6 +1,6 @@
 var cal = require('./calModule.js');
 
-const num = process.argv.slice(2,process.argv.length);
+const num = process.argv.slice(2, process.argv.length);
 //console.log(process.argv.slice(2).length);
 num_length = process.argv.slice(2).length;
 if(num_length > 2 || num_length < 2) {
@@ -9,8 +9,8 @@ if(num_length > 2 || num_length < 2) {
 }
 num[0] = parseInt(num[0]);
 num[1] = parseInt(num[1]);
-console.log("Addition = " + cal.add(num[0],num[1]));
-console.log("Substraction = " + cal.sub(num[0],num[1]));
-console.log("Multiplication = " + cal.mul(num[0],num[1]));
-console.log("Division = " + cal.div(num[0],num[1]));
+console.log("Addition = " + cal.add(num[0], num[1]));
+console.log("Substraction = " + cal.sub(num[0], num[1]));
+console.log("Multiplication = " + cal.mul(num[0], num[1]));
+console.log("Division = " + cal.div(num[0], num[1]));
 
