4abbfe721c3046177780e641f9d1e3f0c6f96678
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-09 14:55:08 +0530

    Coding indentation

--- example4.js
+++ example4.js
@@ -1,18 +1,19 @@
 var fs = require('fs');
 
-fs.readFile('student.js', 'utf8', function(err, data){
-    if (err){
+fs.readFile('student.js', 'utf8', function(err, data) {
+    if (err) {
         console.log(err);
     } else {
     	obj = JSON.parse(data); //now it is an object
     	obj.student.push({"name":"sunil","age":24}); //add data
     	json = JSON.stringify(obj); //convert it back to json
-    
-    fs.writeFile("student.js", json , (err) => {
-    	if (err) {
-        	console.error(err);	
-        	return;
-    	};
-    	console.log("Student recorded added");
+    	
+	fs.writeFile("student.js", json , (err) => {
+    		if (err) {
+        		console.error(err);	
+        		return;
+    		}
+    		console.log("Student recorded added");
 	});
-}});
+    }
+});
