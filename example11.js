ef07cbd0f50eaf3ad9472b6a6c8e71cdbf28b4e7
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-09 14:41:17 +0530

    Coding Indentation

--- example11.js
+++ example11.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get('/student/result/:id', function (req, res) {
-  		if(req.params.id >= 100 && req.params.id <= 200){
+  
+  		if(req.params.id >= 100 && req.params.id <= 200) {
   			res.sendFile(__dirname + '/success.html');
   		}else{
   			res.sendFile(__dirname + '/failure.html');
