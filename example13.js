2226a471438f56f1c5e8ca78bd42aac029de58c6
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-09 13:26:58 +0530

    Coding Indentation

--- example13.js
+++ example13.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const app = express();
 
-
 app.get('/show', function (req, res) {
-  res.status(200).send({"id" : req.query.id,"name": req.query.name});
+  	res.status(200).send({"id" : req.query.id,"name": req.query.name});
 })
 
-
-app.get("*",(req,res)=>{
+app.get("*", (req, res) => {
      	res.status(404).send( {"Message": "invalid information"});
 });
 	
-
-
-app.listen(3000,(rep) => {
-  
+app.listen("3000",(rep) => {
 });
 
 
