74d3cd81ec56d7089848fcde94585c24efa8c458
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-09 13:23:27 +0530

    Coading Intendation

--- example11.js
+++ example11.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 
-
-	app.get('/student/result/:id', function (req, res) {
+app.get('/student/result/:id', function (req, res) {
   		if(req.params.id >= 100 && req.params.id <= 200){
   			res.sendFile(__dirname + '/success.html');
   		}else{
   			res.sendFile(__dirname + '/failure.html');
   		}
-	});
-	
-	app.listen(3000,(rep) => {
-	});
+});
+
+app.listen("3000", (rep) => {
+});
 
 
