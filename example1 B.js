c86a9fc638aa73c05ae001a69a8f638062cc3721
Priyanka12121990 <51879706+Priyanka12121990@users.noreply.github.com>
2019-08-09 13:19:17 +0530

    coding Indentation

--- example1 B.js
+++ example1 B.js
@@ -1,23 +1,22 @@
 //Example 1B using promise
 
-function placeOrder(ex_time, order_id){
+function placeOrder(ex_time, order_id) {
 	
-	return new Promise(function (resolve,reject) {
+	return new Promise(function (resolve, reject) {
 			console.log(`receive order`+ order_id);
-			setTimeout(()=>{
-				
+			setTimeout(() => {
 				resolve(console.log("delivery order"+ order_id));
-			},ex_time);
+			}, ex_time);
 	});
 }
 
-placeOrder(5000, 1).then((resolve) =>{
-  	placeOrder(4000, 2).then((resolve) =>{
-  		placeOrder(1000, 3).then((resolve) =>{
-  			placeOrder(2000, 4).then((resolve) =>{
+placeOrder(5000, 1).then((resolve) => {
+  	placeOrder(4000, 2).then((resolve) => {
+  		placeOrder(1000, 3).then((resolve) => {
+  			placeOrder(2000, 4).then((resolve) => {
 			});
 		});
 	});
-},(error) =>{
+}, (error) => {
   console.log(error)
 });
